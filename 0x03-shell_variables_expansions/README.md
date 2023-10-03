97038b87e1581387cd38fcf7e9679ea92fe88461
Shamsudeen Ibrahim <shamsain3@gmail.com>
2023-10-03 08:58:33 -0700

    added new files

---
+++ 0x03-shell_variables_expansions/README.md
@@ -0,0 +1,4 @@
+# Shell Variables and Expansions
+
+A project to explore the different areas and solve problems related to shell variables arithmetics and expansion
+
---
+++ 0x03-shell_variables_expansions/automation.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+vi README.md
+vi 0-alias
+vi 1-hello_you
+vi 2-path
+vi 3-paths
+vi 4-global_variables
+vi 5-local_variables
+vi 6-create_local_variable
+vi 7-create_global_variable
+vi 8-true_knowledge
+vi 9-divide_and_rule
+vi 10-love_exponent_breath
+chmod 777 0-alias 1-hello_you 2-path 3-paths 4-global_variables 5-local_variables 6-create_local_variable 7-create_global_variable 8-true_knowledge 9-divide_and_rule 10-love_exponent_breath
+git add .
+git commit -m "added new files"
+git push
