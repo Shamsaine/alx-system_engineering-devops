62db9ce7e12a55a0273864c7ab82935665667204
Shamsudeen Ibrahim <shamsain3@gmail.com>
2023-10-03 06:47:06 -0700

    Added a new file

--- 0x02-shell_redirections/automation.sh
+++ 0x02-shell_redirections/automation.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
-umask 000
+vi 10-no_more_js
+vi 11-directories
+vi 12-newest_files
+chmod 777 10-no_more_js 11-directories 12-newest_files
 git add .
 git commit -m "Added a new file"
 git push
